88773e4d589ba3c1fe0a45ee63232f22cf468847
Carlos Kimura <ckimura@gmail.com>
2023-11-29 10:09:40 -0300

    UITabBar and Search and Favorites VC added

---
+++ GitHubFollowers/FavoritesListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesListViewController.swift
+//  GitHubFollowers
+//
+//  Created by Carlos Kimura on 29/11/23.
+//
+
+import UIKit
+
+class FavoritesListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
--- GitHubFollowers/SearchViewController.swift
+++ GitHubFollowers/SearchViewController.swift
@@ -1,19 +1,17 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  GitHubFollowers
 //
-//  Created by Carlos Kimura on 28/11/23.
+//  Created by Carlos Kimura on 29/11/23.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemPink
     }
 
-
 }
-
