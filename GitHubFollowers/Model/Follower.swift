376a6745c310bfcd696dafabca951c848054fc87
Carlos Kimura <ckimura@gmail.com>
2023-11-30 10:47:23 -0300

    follower and user model created

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Carlos Kimura on 30/11/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
