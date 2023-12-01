995f8e5771e71de382f0c2b4821f4ec44bc7a21b
Carlos Kimura <ckimura@gmail.com>
2023-12-01 15:52:42 -0300

    user info header created

---
+++ GitHubFollowers/Util/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Carlos Kimura on 01/12/23.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
