d0396053693050ed62780c9f351ada67daca3adc
Carlos Kimura <ckimura@gmail.com>
2023-12-02 17:13:48 -0300

    add button added to follower list view controller

--- GitHubFollowers/Screens/FollowerListViewController.swift
+++ GitHubFollowers/Screens/FollowerListViewController.swift
@@ -44,6 +44,9 @@ class FollowerListViewController: UIViewController {
     func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addButtonTapped))
+        navigationItem.rightBarButtonItem = addButton
     }
     
     func configureCollectionView() {
@@ -99,6 +102,10 @@ class FollowerListViewController: UIViewController {
         snapshot.appendSections([.main])
         snapshot.appendItems(followers)
         DispatchQueue.main.async { self.dataSource.apply(snapshot, animatingDifferences: true) }
+    }
+    
+    @objc func addButtonTapped() {
+        print("add button tapped")
     }
 }
 
