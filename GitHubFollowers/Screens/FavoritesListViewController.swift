588c5ffcd66a5c88579e064c93f4a7b1f3fb8aea
Carlos Kimura <ckimura@gmail.com>
2023-12-04 10:49:10 -0300

    swipe to delete favorites from tableview

--- GitHubFollowers/Screens/FavoritesListViewController.swift
+++ GitHubFollowers/Screens/FavoritesListViewController.swift
@@ -78,6 +78,30 @@ extension FavoritesListViewController: UITableViewDelegate, UITableViewDataSourc
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let favorite = favorites[indexPath.row]
+        
+        let destVC = FollowerListViewController()
+        destVC.username = favorite.login
+        destVC.title = favorite.login
+        
+        navigationController?.pushViewController(destVC, animated: true)
+    }
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else { return }
+        
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        
+        tableView.deleteRows(at: [indexPath], with: .left)
+        
+        PersistenceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else { return }
+            guard let error = error else { return }
+            
+            self.presentGithubAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
     
 }
