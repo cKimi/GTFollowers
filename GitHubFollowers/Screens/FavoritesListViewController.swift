54e36b24276268ef92b46f1632638a538dadd4b1
Carlos Kimura <ckimura@gmail.com>
2023-12-05 14:26:40 -0300

    guard let statements and bugfix

--- GitHubFollowers/Screens/FavoritesListViewController.swift
+++ GitHubFollowers/Screens/FavoritesListViewController.swift
@@ -42,7 +42,7 @@ class FavoritesListViewController: GitHubDataLoadingViewController {
     
     func getFavorites() {
         PersistenceManager.retrieveFavorites { [weak self] result in
-            guard let self = self else { return }
+            guard let self else { return }
             
             switch result {
             case .success(let favorites):
@@ -93,10 +93,13 @@ extension FavoritesListViewController: UITableViewDelegate, UITableViewDataSourc
         guard editingStyle == .delete else { return }
         
         PersistenceManager.updateWith(favorite: favorites[indexPath.row], actionType: .remove) { [weak self] error in
-            guard let self = self else { return }
-            guard let error = error else {
+            guard let self else { return }
+            guard let error else {
                 self.favorites.remove(at: indexPath.row)
                 tableView.deleteRows(at: [indexPath], with: .left)
+                if self.favorites.isEmpty {
+                    self.showEmptyStateView(with: "No favorites?\nAdd one on the follower screen.", in: self.view)
+                }
                 return
             }
             
